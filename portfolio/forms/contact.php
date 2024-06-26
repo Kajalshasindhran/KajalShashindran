ec3aef05e4e87866a06f269f85f64a29584479a7
kajal <kajalshasindhran2023@gmail.com>
2024-06-26 09:26:56 +0100

    Portfolio source code

---
+++ portfolio/forms/contact.php
@@ -0,0 +1,38 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require '../vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+
+    //$mail->SMTPDebug = 0;                                 
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                     
+    $mail->isSMTP();                                           
+    $mail->Host       = 'box5181.bluehost.com';          
+    $mail->SMTPAuth   = true;                                   
+    $mail->Username   = 'info@kajalshashindran.co.uk';                     
+    $mail->Password   = '123456';                              
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; 
+    $mail->Port       = 465;                                    
+
+    $mail->setFrom($_POST['email'], 'Mailer');     //Add a recipient
+    $mail->addAddress('info@kajalshashindran.co.uk');
+
+    //Content
+    $mail->isHTML(true);                                  
+    $mail->Subject = $_POST['subject'];
+    $mail->Body    = $_POST['message'];
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'OK';
+    
+} catch (Exception $e) {
+  echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
